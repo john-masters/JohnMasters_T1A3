e8590764b4025e61be95ed9afc4bee01af1f2836
john-masters <johnmasters@email.com>
2022-09-25 18:09:23 +1000

    Added conditionals checking for python version in bash script

--- src/game.sh
+++ src/game.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-python3 main.py
+if [[ -x "$(command -v python)" ]]
+then
+    pyv="$(python -V 2>&1)"
+    if [[ $pyv == "Python 3"* ]]
+    then
+        python3 main.py
+    else
+        echo "You've got the wrong version of python, sort it out!" >&2
+    fi 
+else
+    echo 'Error: 
+    This program runs on Python, but it looks like Python is not installed.
+    To install Python, check out https://installpython3.com/' >&2
+fi
